b8c599f04c945416e1a58ffe82fe573cb69b6fb5
r41L <stephanbrummel@online.de>
2018-01-13 22:06:52 +0100

    import frame

---
+++ src/main/java/Forms/frmImportInvoice.java
@@ -0,0 +1,12 @@
+package Forms;
+
+import javax.swing.*;
+
+public class frmImportInvoice {
+    private JTextField lieferantTextField;
+    private JButton speichernButton;
+    private JTable table1;
+    private JLabel bruttoLabel;
+    private JLabel mwstLabel;
+    private JLabel nettoLabel;
+}
