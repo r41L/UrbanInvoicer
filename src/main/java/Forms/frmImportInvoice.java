7eb95c8e5cdd7f56e5306df1fe4385f8af024ba6
Cataklysim <ig-100@web.de>
2018-01-13 23:22:33 +0100

    Buttons for everyone!
    Every Button in the main form now works!
    Wuhuuuuuu

--- src/main/java/Forms/frmImportInvoice.java
+++ src/main/java/Forms/frmImportInvoice.java
@@ -2,11 +2,26 @@ package Forms;
 
 import javax.swing.*;
 
-public class frmImportInvoice {
+public class frmImportInvoice  extends JInternalFrame{
     private JTextField lieferantTextField;
     private JButton speichernButton;
     private JTable table1;
     private JLabel bruttoLabel;
     private JLabel mwstLabel;
     private JLabel nettoLabel;
+    private JPanel mainPanel;
+    static final int xOffset = 30, yOffset = 30;
+    int inset = 50;
+
+    public frmImportInvoice(int pWidth, int pHeight){
+        super("UrbanInvoicing - Rechnung Eingehend",
+                false, //resizable
+                false, //closable
+                false, //maximizable
+                false);//iconifiable
+        setSize(pWidth, pHeight);
+        //frmContainer.openFrameCount = frmContainer.openFrameCount+1;
+        setLocation(xOffset * 1, yOffset * 1);
+        setContentPane(mainPanel);
+    }
 }
--- src/main/java/Forms/frmNewAddress.java
+++ src/main/java/Forms/frmNewAddress.java
@@ -2,6 +2,12 @@ package Forms;
 
 import javax.swing.*;
 
-public class frmNewAddress {
+public class frmNewAddress  extends JInternalFrame{
     private JTextField vornameTextField;
+    static final int xOffset = 30, yOffset = 30;
+    int inset = 50;
+
+    public frmNewAddress(int pWidth, int pHeight){
+
+    }
 }
