31310edc4185d5144da5d8f76b29f1899e74c22c
r41L <stephanbrummel@online.de>
2018-01-15 10:53:28 +0100

    frmNewAddress

--- src/main/java/Forms/frmNewAddress.java
+++ src/main/java/Forms/frmNewAddress.java
@@ -4,4 +4,16 @@ import javax.swing.*;
 
 public class frmNewAddress {
     private JTextField vornameTextField;
+    private JTextField nachnameTextField;
+    private JTextField strasseTextField;
+    private JTextField plzTextField;
+    private JTextField ortTextField;
+    private JTextField landTextField;
+    private JTextField telefoneTextField;
+    private JTextField telefaxTextField;
+    private JTextField emailTextField;
+    private JTextArea bemerkungTextArea;
+    private JButton speichernButton;
+    private JTable table1;
+    private JButton zurückZumHauptmenüButton;
 }
